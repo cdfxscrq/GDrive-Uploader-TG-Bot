e3fc586e53b01692afba6375452a358d1cda1fe0
AK25 <60230389+cdfxscrq@users.noreply.github.com>
2025-06-06 13:37:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,21 @@
 from pyrogram import Client
 from config import Config
 
-
+# Plugins directory
 plugins = dict(root="plugins")
 
+def main():
+    try:
+        app = Client(
+            "GDrive-Uploader-TG-Bot",
+            bot_token=Config.BOT_TOKEN,
+            api_id=Config.APP_ID,
+            api_hash=Config.API_HASH,
+            plugins=plugins
+        )
+        app.run()
+    except Exception as e:
+        print(f"[ERROR] {e}")
 
-app = Client('GDrive-Bot',
-        bot_token = Config.BOT_TOKEN,
-        api_id = Config.APP_ID,
-        api_hash = Config.API_HASH,
-        plugins = plugins
-      )
-
-
-app.run()
+if __name__ == "__main__":
+    main()
